9d9f5044eb5c0b6b25f72a7cc4fe4551ffe7e17c
JunFe23 <ogum0405@hanmail.net>
2024-07-23 23:29:11 +0900

    Initial commit

---
+++ src/main/java/org/example/g2bplatform/G2BPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.g2bplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2BPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G2BPlatformApplication.class, args);
+    }
+
+}
