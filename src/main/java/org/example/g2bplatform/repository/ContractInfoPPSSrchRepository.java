f2e3ffb00f085cdb40026df6d67feb7497a0816d
JunFe23 <ogum0405@hanmail.net>
2025-05-27 23:57:45 +0900

    frontend, backend 분리

--- src/main/java/org/example/g2bplatform/repository/ContractInfoPPSSrchRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.g2bplatform.repository;
-
-import org.example.g2bplatform.model.ContractInfoPPSSrch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContractInfoPPSSrchRepository extends JpaRepository<ContractInfoPPSSrch, Long> {
-}
--- src/main/java/org/example/g2bplatform/repository/DownloadHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.g2bplatform.repository;
-
-import org.example.g2bplatform.model.DownloadHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DownloadHistoryRepository extends JpaRepository<DownloadHistory, Long> {
-}
