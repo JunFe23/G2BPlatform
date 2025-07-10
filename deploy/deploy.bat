027decd95944b2f40a98db02842a0fd25170a77c
JunFe23 <ogum0405@hanmail.net>
2025-07-11 00:04:06 +0900

    배포전용 프로젝트 삭제

--- deploy/deploy.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-git pull
-cd backend
-gradlew.bat bootJar
-docker-compose down
-docker-compose up --build -d
--- deploy/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd ../
-git pull
-cd backend
-./gradlew bootJar
-docker-compose down
-docker-compose up --build -d
